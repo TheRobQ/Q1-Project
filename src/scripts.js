77b026d67ab7731f8e4e7a4e90a235f8b3014d80
Robert Quan <rob.quan@gmail.com>
2017-10-27 21:28:31 -0600

    Basic functions for printing random numbers to DOM work

--- src/scripts.js
+++ src/scripts.js
@@ -2,29 +2,31 @@ $(document).ready(function() {
   var initValue = [];
   var actor = [];
   var totals = [];
-  function PrintOut(name, value){
-    this.name = name;
-    this.value = value;
-  };
+//ary.push({[name]: val});
+  // function PrintOut(name, inits) {
+  //   this.name = name;
+  //   this.inits = inits;
+  // };
 
   $('.btn').click(function(event) {
     $(".modifier").each(function() {
-      initValue.push(parseInt($(this).val())
-      + Math.floor(Math.random() * 20));
+      initValue.push(parseInt($(this).val()) +
+        Math.floor(Math.random() * 20));
     })
-    $(".characterName").each(function(event){
+    $(".characterName").each(function(event) {
       actor.push($(this).val())
     })
-    console.log(initValue);
-    for(let i = 0; i< actor.length; i++){
-      totals.push(new PrintOut(actor[i], initValue[i]))
+    //console.log(initValue);
+    for (let i = 0; i < actor.length; i++) {
+      totals.push({["name"]: actor[i], ["inVal"]: initValue[i]})
     }
-    totals.sort(function(a, b){
-      return a.value - b.value;
+    totals.sort(function(a, b) {
+      return a.inVal - b.inVal;
     }).reverse();
-    //console.log(totals);
-
-    })
+    console.log(totals);
+    for(let j =0; j < totals.length; j++){
+    $('.list').append("<p>" + totals[j]["name"] + " " + totals[j]["inVal"] + "</p>")}
+  })
 
 
 
