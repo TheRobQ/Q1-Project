9c039345bba841f286032740104ad53d50eb47e0
Robert Quan <rob.quan@gmail.com>
2017-10-27 20:19:11 -0600

    Basic functions for gathering user inputs work

--- src/scripts.js
+++ src/scripts.js
@@ -1,16 +1,29 @@
 $(document).ready(function() {
-  var initMods = [];
-  var actor = {};
-
+  var initValue = [];
+  var actor = [];
+  var totals = [];
+  function PrintOut(name, value){
+    this.name = name;
+    this.value = value;
+  };
 
   $('.btn').click(function(event) {
     $(".modifier").each(function() {
-      initMods.push(parseInt($(this).val()))
+      initValue.push(parseInt($(this).val())
+      + Math.floor(Math.random() * 20));
     })
-    console.log(initMods);
-    var initValue = initMods.map(function(x){
-      return x + Math.floor(Math.random() * 20)});
+    $(".characterName").each(function(event){
+      actor.push($(this).val())
+    })
     console.log(initValue);
+    for(let i = 0; i< actor.length; i++){
+      totals.push(new PrintOut(actor[i], initValue[i]))
+    }
+    totals.sort(function(a, b){
+      return a.value - b.value;
+    }).reverse();
+    //console.log(totals);
+
     })
 
 
