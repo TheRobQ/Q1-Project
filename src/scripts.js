ca0a8fc09a4d4e40ad92440908718a132d9d508f
Robert Quan <rob.quan@gmail.com>
2017-10-30 11:17:01 -0600

    Basic functions to add and remove are in place

--- src/scripts.js
+++ src/scripts.js
@@ -2,35 +2,39 @@ $(document).ready(function() {
   var initValue = [];
   var actor = [];
   var totals = [];
-//ary.push({[name]: val});
-  // function PrintOut(name, inits) {
-  //   this.name = name;
-  //   this.inits = inits;
-  // };
 
-  $('.btn').click(function(event) {
+  $('#addCharacter').click(function(event){
+    $('#buttonRow').before('<tr class="inputRow"><td><label>Character/Foe </label></td>' +
+    '<td><input type="text" value="" class="characterName" required>' + '<label class="modLabel">Modifier</label></td>' +
+    '<td><input type="text" class="modifier" maxlength="3" size="3" required pattern="[1-9]"></td></tr>')
+  })
+
+  $('#axCharacter').click(function(event){
+    $('.inputRow:last').remove();
+  })
+
+  $('#form1').submit(function(event) {
+    event.preventDefault();
     $(".modifier").each(function() {
       initValue.push(parseInt($(this).val()) +
-        Math.floor(Math.random() * 20));
+        Math.floor((Math.random() * 20) + 1))
     })
     $(".characterName").each(function(event) {
       actor.push($(this).val())
     })
-    //console.log(initValue);
+
     for (let i = 0; i < actor.length; i++) {
-      totals.push({["name"]: actor[i], ["inVal"]: initValue[i]})
+      totals.push({
+        ["name"]: actor[i],
+        ["inVal"]: initValue[i]
+      })
     }
     totals.sort(function(a, b) {
       return a.inVal - b.inVal;
     }).reverse();
     console.log(totals);
-    for(let j =0; j < totals.length; j++){
-    $('.list').append("<p>" + totals[j]["name"] + " " + totals[j]["inVal"] + "</p>")}
+    for (let j = 0; j < totals.length; j++) {
+      $('.list').append("<p>" + totals[j]["name"] + " " + totals[j]["inVal"] + "</p>")}
   })
-
-
-
-
-
 
 })
